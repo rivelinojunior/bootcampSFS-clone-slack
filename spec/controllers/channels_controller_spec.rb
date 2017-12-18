0e219b48b6ef1ef2aed57fd1aa832f9c9e477788
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-12-18 16:15:28 -0400

    feat: test action show to channels_controller

--- spec/controllers/channels_controller_spec.rb
+++ spec/controllers/channels_controller_spec.rb
@@ -50,7 +50,59 @@ RSpec.describe ChannelsController, type: :controller do
         post :create, params: { channel: @channel_attributes.merge(team_id: @team.id) }
       end
 
-      it "returns http forbidden" do
+      it 'returns http forbidden' do
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    # Sem isto os testes não renderizam o json
+    render_views
+
+    context 'User is team member' do
+      before(:each) do
+        team = create(:team, user: @current_user)
+        @channel = create(:channel, team: team)
+
+        @message1 = build(:message)
+        @message2 = build(:message)
+
+        @channel.messages << [@message1, @message2]
+        get :show, params: { id: @channel.id }
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns right channel values' do
+        response_hash = JSON.parse(response.body)
+
+        expect(response_hash['slug']).to eql(@channel.slug)
+        expect(response_hash['user_id']).to eql(@channel.user.id)
+        expect(response_hash['team_id']).to eql(@channel.team.id)
+      end
+
+      it 'Return the right number of messages' do
+        response_hash = JSON.parse(response.body)
+        expect(response_hash['messages'].count).to eql(2)
+      end
+
+      it 'Return the right messages' do
+        response_hash = JSON.parse(response.body)
+        expect(response_hash['messages'][0]['body']).to eql(@message1.body)
+        expect(response_hash['messages'][0]['user_id']).to eql(@message1.user.id)
+        expect(response_hash['messages'][1]['body']).to eql(@message2.body)
+        expect(response_hash['messages'][1]['user_id']).to eql(@message2.user.id)
+      end
+    end
+
+    context 'User is not team member' do
+      it 'returns http forbidden' do
+        channel = create(:channel)
+        get :show, params: { id: channel.id }
+
         expect(response).to have_http_status(:forbidden)
       end
     end
