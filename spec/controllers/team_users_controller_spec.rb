457cbc097fbb3c58c3a3d9966256d0f6fe361e01
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-12-18 16:33:16 -0400

    feat: test action create to team_users__controller

---
+++ spec/controllers/team_users_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe TeamUsersController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
+  before(:each) do
+    request.env['HTTP_ACCEPT'] = 'application/json'
+
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+    @current_user = FactoryGirl.create(:user)
+    sign_in @current_user
+  end
+
+  describe 'GET #create' do
+    context 'Team owner' do
+      it 'returns http success' do
+        @team = create(:team, user: @current_user)
+        @guest_user = create(:user)
+        post :create, params: { team_user: { user_id: @guest_user.id, team_id: @team.id } }
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'Team not owner' do
+      it 'returns http forbidden' do
+        @team = create(:team)
+        @guest_user = create(:user)
+        post :create, params: { team_user: { user_id: @guest_user.id, team_id: @team.id } }
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
+end
