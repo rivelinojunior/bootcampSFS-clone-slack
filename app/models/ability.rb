2b0ac8c3a6febe4cd9efee4826a6244fd8249241
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-12-18 16:50:01 -0400

    feat: set rules with cancancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,10 +3,31 @@ class Ability
 
   def initialize(user)
     if user
+      #Team ---
       can :read, Team do |t|
         t.user_id == user.id || t.users.where(id: user.id).present?
       end
+
+      can :destroy, Team, user_id: user.id
+
+      #Channel ---
+      can [:read, :create], Channel do |c|
+        c.team.user_id == user.id || c.team.users.where(id: user.id).present?
+      end
+
+      can [:destroy, :update], Channel do |c|
+        c.team.user_id == user.id || c.user_id == user.id
+      end
+
+      #Talk ---
+      can [:read], Talk do |t|
+        t.user_one_id == user.id || t.user_two_id == user.id
+      end
+
+      #TeanUser ---
+      can [:create, :destroy], TeamUser do |t|
+        t.team.user_id == user.id
+      end
     end
   end
-
 end
