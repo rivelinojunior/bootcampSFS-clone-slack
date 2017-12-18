448a4568ac96d6674e2a5da474f0766f976ccfe4
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-12-18 16:21:27 -0400

    feat: test action destroy to channels_controller

--- spec/controllers/channels_controller_spec.rb
+++ spec/controllers/channels_controller_spec.rb
@@ -107,4 +107,52 @@ RSpec.describe ChannelsController, type: :controller do
       end
     end
   end
+
+  describe 'DELETE #destroy' do
+    context 'User is team member' do
+      context 'User is the channel owner' do
+        it 'returns http success' do
+          team = create(:team)
+          team.users << @current_user
+          @channel = create(:channel, team: team, user: @current_user)
+
+          delete :destroy, params: { id: @channel.id }
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context 'User is the team owner' do
+        it 'returns http success' do
+          team = create(:team, user: @current_user)
+          channel_owner = create(:user)
+          team.users << channel_owner
+          @channel = create(:channel, team: team, user: channel_owner)
+
+          delete :destroy, params: { id: @channel.id }
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context "User isn't the team or channel owner" do
+        it 'returns http forbidden' do
+          team = create(:team)
+          team.users << @current_user
+          @channel = create(:channel, team: team)
+
+          delete :destroy, params: { id: @channel.id }
+          expect(response).to have_http_status(:forbidden)
+        end
+      end
+    end
+
+    context "User isn't team member" do
+      it 'returns http forbidden' do
+        team = create(:team)
+        @channel = create(:channel, team: team)
+
+        delete :destroy, params: { id: @channel.id }
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
 end
