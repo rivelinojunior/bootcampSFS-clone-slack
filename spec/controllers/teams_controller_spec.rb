20b0852998957ffb3019298e8e55692f10f05ed6
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-12-18 15:24:01 -0400

    feat: test action destroy to teams_controller

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -73,4 +73,35 @@ RSpec.describe TeamsController, type: :controller do
       expect(Team.last.slug).to eql(@team_attributes[:slug])
     end
   end
+
+  describe 'DELETE #destroy' do
+    before(:each) do
+      request.env['HTTP_ACCEPT'] = 'application/json'
+    end
+
+    context 'User is the Team Owner' do
+      it 'returns http success' do
+        team = create(:team, user: @current_user)
+        delete :destroy, params: { id: team.id }
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "User isn't the team Owner" do
+      it 'returns http forbidden' do
+        team = create(:team)
+        delete :destroy, params: { id: team.id }
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+
+    context 'User is member of the team' do
+      it 'returns http forbidden' do
+        team = create(:team)
+        team.users << @current_user
+        delete :destroy, params: { id: team.id }
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
 end
