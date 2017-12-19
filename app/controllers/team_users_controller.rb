6ff675052d9b4d145cda88bc97279a97d701b9b5
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-12-19 17:22:25 -0400

    feat: create action destroy #delete to team_users_controller

--- app/controllers/team_users_controller.rb
+++ app/controllers/team_users_controller.rb
@@ -1,7 +1,21 @@
 class TeamUsersController < ApplicationController
+  before_action :set_team_user, only: [:destroy]
+
   def create
   end
 
   def destroy
+    authorize! :destroy, @team_user
+    @team_user.destroy
+
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+  private 
+
+  def set_team_user
+    @team_user = TeamUser.find_by(params[:user_id], params[:team_id])
   end
 end
