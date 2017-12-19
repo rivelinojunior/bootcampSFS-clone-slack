a6ddee3d06955019c9d7afb6176fb1c3d073f7c2
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-12-19 17:25:23 -0400

    refactor: adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,9 @@
 Rails.application.routes.draw do
-  get 'team_users/create'
-
-  get 'team_users/destroy'
-
-  get 'talks/show'
-
-  get 'channels/create'
-
-  get 'channels/destroy'
-
-  get 'channels/show'
-
-  devise_for :users, :controllers => { registrations: 'registrations' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root to: 'teams#index'
-  resources :teams, only: [:create, :destroy]
   get '/:slug', to: 'teams#show'
-  
+  resources :teams, only: [:create, :destroy]
+  resources :channels, only: [:show, :create, :destroy]
+  resources :talks, only: [:show]
+  resources :team_users, only: [:create, :destroy]
+  devise_for :users, :controllers => { registrations: 'registrations' }
 end
