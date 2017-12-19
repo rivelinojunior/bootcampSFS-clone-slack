4c90abf5b537ff5e67157669e1cf5432f27784c2
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-12-19 16:55:04 -0400

    feat: create action destroy #delete to channels_controller

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,10 +1,24 @@
 class ChannelsController < ApplicationController
+  before_action :set_channel, only: %i[destroy show]
+
   def create
   end
 
   def destroy
+    authorize! :destroy, @channel
+    @channel.destroy
+
+    respond_to do |format|
+      format.json { head :no_content }
+    end
   end
 
   def show
+  end
+
+  private
+
+  def set_channel
+    @channel = Channel.find(params[:id])
   end
 end
