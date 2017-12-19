4bf0b2428937b7631eb6e5cf91494a367689068f
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-12-19 16:56:45 -0400

    feat: create action show #get and create #post to channels_controller

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,6 +2,16 @@ class ChannelsController < ApplicationController
   before_action :set_channel, only: %i[destroy show]
 
   def create
+    @channel = Channel.new(channel_params)
+    authorize! :create, @channel
+
+    respond_to do |format|
+      if @channel.save
+        format.json { render :show, status: :created }
+      else
+        format.json { render json: @channel.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
@@ -14,11 +24,16 @@ class ChannelsController < ApplicationController
   end
 
   def show
+    authorize! :read, @channel
   end
 
   private
 
   def set_channel
     @channel = Channel.find(params[:id])
+  end
+
+  def channel_params
+    params.require(:channel).permit(:slug, :team_id).merge(user: current_user)
   end
 end
