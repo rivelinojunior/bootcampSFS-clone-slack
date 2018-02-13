4e3331877ecaf6988e0458f28c845a5aacf40730
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-02-13 13:36:00 -0400

    add test for the user to exit of team

--- spec/controllers/team_users_controller_spec.rb
+++ spec/controllers/team_users_controller_spec.rb
@@ -66,13 +66,29 @@ RSpec.describe TeamUsersController, type: :controller do
     context 'Team not owner' do
       before(:each) do
         @team = create(:team)
-        @guest_user = create(:user)
-        @team.users << @guest_user
       end
 
-      it 'returns http forbidden' do
-        delete :destroy, params: { id: @guest_user.id, team_id: @team.id }
-        expect(response).to have_http_status(:forbidden)
+      context 'remove youself' do
+        before(:each) do
+          @team.users << @current_user #guest_user
+        end
+
+        it 'returns http success' do
+          delete :destroy, params: {id: @current_user.id, team_id: @team.id}
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context 'remove other user' do
+        before(:each) do
+          @guest_user = create(:user)
+          @team.users << @guest_user
+        end
+        
+        it 'returns http forbidden' do
+          delete :destroy, params: { id: @guest_user.id, team_id: @team.id }
+          expect(response).to have_http_status(:forbidden)
+        end
       end
     end
   end
