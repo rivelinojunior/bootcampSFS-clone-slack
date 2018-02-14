71738d251902335f4fc7c87fd723c57091c997d1
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-02-14 11:39:29 -0400

    change the team_users params

--- app/controllers/team_users_controller.rb
+++ app/controllers/team_users_controller.rb
@@ -26,7 +26,8 @@ class TeamUsersController < ApplicationController
   private
 
   def set_team_user
-    @team_user = TeamUser.find_by(params[:user_id], params[:team_id])
+    params[:user_id] ||= params[:id]
+    @team_user = TeamUser.where(user_id: params[:user_id], team_id: params[:team_id]).first
   end
 
   def team_user_params
