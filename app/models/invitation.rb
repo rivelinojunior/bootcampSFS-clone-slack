0fe23b7ed930a5824b8e0ef85b3fda2e069f2535
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-02-15 18:39:35 -0300

    create in invitation model callback for after_update add the guest with member of the team

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,4 +2,13 @@ class Invitation < ApplicationRecord
   belongs_to :user
   belongs_to :team
   belongs_to :guest, class_name: 'User', foreign_key: 'guest_id'
+  after_update :add_guest_with_member_team
+
+
+  private 
+    def add_guest_with_member_team
+      if self.approved
+        self.team.team_users.create(user_id: self.guest.id)
+      end
+    end
 end
