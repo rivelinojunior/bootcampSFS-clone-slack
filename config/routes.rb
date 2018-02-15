1fcf36c499b00633f6fc5bdd73a8b4966f12c7c8
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-02-15 19:19:57 -0300

    add route destroy for invitation

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   resources :channels, only: [:show, :create, :destroy]
   resources :talks, only: [:show]
   resources :team_users, only: [:create, :destroy]
-  resources :invitations, only: [:create, :update, :index]
+  resources :invitations, only: [:create, :update, :index, :destroy]
   devise_for :users, :controllers => { registrations: 'registrations' }
 end
