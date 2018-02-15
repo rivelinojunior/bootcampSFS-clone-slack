a7f78709a2a1610366d99e7b499e6d7c154e8b35
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-02-14 22:08:36 -0400

    create routes(index, create, update) for invitations

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :channels, only: [:show, :create, :destroy]
   resources :talks, only: [:show]
   resources :team_users, only: [:create, :destroy]
+  resources :invitations, only: [:create, :update, :index]
   devise_for :users, :controllers => { registrations: 'registrations' }
 end
