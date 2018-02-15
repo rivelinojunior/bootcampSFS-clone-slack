949d752193aa27dd18c55a1050acc039698e5ce0
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-02-15 18:37:16 -0300

    add in application controller rescue from expection not found of active record

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
   include CanCan::ControllerAdditions
   
   rescue_from CanCan::AccessDenied do |exception|
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
       format.json { head :forbidden, content_type: 'text/html' }
       format.html { redirect_to main_app.root_url, notice: exception.message }
     end
+  end
+
+  def render_404
+    redirect_to main_app.root_url, status: 404
   end
 end
