984a951929339fe4223eb8189ba321413e0af719
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-02-16 00:45:13 -0300

    add scope (approved: false) in invitations of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :talks, dependent: :destroy
   has_many :team_users, dependent: :destroy
   has_many :member_teams, through: :team_users, :source => :team
-  has_many :invitations, class_name: 'Invitation', foreign_key: 'guest_id'
+  has_many :invitations, -> { where approved: false }, class_name: 'Invitation', foreign_key: 'guest_id'
   has_many :inviting, class_name: 'Invitation', foreign_key: 'user_id'
 
   devise :database_authenticatable, :registerable,
