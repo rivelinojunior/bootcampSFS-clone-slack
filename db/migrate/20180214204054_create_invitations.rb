7db7f72b39cd8e3f46c73ae3d73b267dace8a69b
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-02-16 00:44:26 -0300

    add default value (false) for approved in invitations

--- db/migrate/20180214204054_create_invitations.rb
+++ db/migrate/20180214204054_create_invitations.rb
@@ -4,7 +4,7 @@ class CreateInvitations < ActiveRecord::Migration[5.0]
       t.integer :guest_id
       t.references :user, foreign_key: true
       t.references :team, foreign_key: true
-      t.boolean :approved
+      t.boolean :approved, default: false
 
       t.timestamps
     end
