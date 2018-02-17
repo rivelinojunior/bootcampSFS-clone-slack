e56b575f092126d8ae402569a65681b646db4e3b
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-02-17 02:57:13 -0300

    generate avatar uploader

---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,21 @@
+class AvatarUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def default_url(*_args)
+    'uploads/avatar_default.jpeg'
+  end
+
+  version :thumb do
+    process resize_to_fit: [200, 200]
+  end
+
+  def extension_whitelist
+     %w(jpg jpeg png)
+  end
+end
