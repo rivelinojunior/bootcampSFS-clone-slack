53ce4c77e803e623a35d91f37f00fe594fd43c17
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-02-17 03:30:07 -0300

    fix path default avatar

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,7 +4,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def default_url(*_args)
