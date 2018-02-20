8f14249cd174af120db9c3bc5b44608c6efe40e6
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-02-20 14:41:21 -0400

    add callback after_save for to add the ids of the users

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,13 @@ class Message < ApplicationRecord
   validates_presence_of :body, :user
 
   after_create_commit {MessageBroadcastJob.perform_later self}
+  after_create_commit :notify_users
+  
+  private
+
+    def notify_users
+      self.messagable.notify_users = self.messagable.team.my_users.pluck(:id)
+      self.messagable.notify_users.delete self.user.id
+      self.messagable.save
+    end
 end
